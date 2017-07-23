a3ba964dc0d4688d6d55394eec5e11667165be0d
Adam Buechler <butchler@gmail.com>
2017-07-23 17:34:30 +0900

    Set up karma with headless chrome

--- packages/s-is-for-style/src/convertStyleDescriptionToRuleDescriptions.test.js
+++ packages/s-is-for-style/src/convertStyleDescriptionToRuleDescriptions.test.js
@@ -1,35 +1,37 @@
 import convertStyleDescriptionToRuleDescriptions from './convertStyleDescriptionToRuleDescriptions';
 
-test('it works', () => {
-  expect(convertStyleDescriptionToRuleDescriptions('a', {
-    border: '1px solid black',
-    borderRadius: '5px',
-    ':hover': {
-      border: '1px solid red',
-    },
-    borderTop: '3px solid blue',
-  })).toEqual([
-    {
-      ruleType: 'style',
-      ruleKey: '.a',
-      declarations: {
-        border: '1px solid black',
-        borderRadius: '5px',
-      },
-    },
-    {
-      ruleType: 'style',
-      ruleKey: '.a:hover',
-      declarations: {
+describe('convertStyleDescriptionToRuleDescriptions', () => {
+  it('works', () => {
+    expect(convertStyleDescriptionToRuleDescriptions('a', {
+      border: '1px solid black',
+      borderRadius: '5px',
+      ':hover': {
         border: '1px solid red',
       },
-    },
-    {
-      ruleType: 'style',
-      ruleKey: '.a',
-      declarations: {
-        borderTop: '3px solid blue',
+      borderTop: '3px solid blue',
+    })).to.eql([
+      {
+        ruleType: 'style',
+        ruleKey: '.a',
+        declarations: {
+          border: '1px solid black',
+          borderRadius: '5px',
+        },
       },
-    },
-  ]);
+      {
+        ruleType: 'style',
+        ruleKey: '.a:hover',
+        declarations: {
+          border: '1px solid red',
+        },
+      },
+      {
+        ruleType: 'style',
+        ruleKey: '.a',
+        declarations: {
+          borderTop: '3px solid blue',
+        },
+      },
+    ]);
+  });
 });
