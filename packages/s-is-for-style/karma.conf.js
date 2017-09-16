ae2d72b42fc4518983204f6e7ebae8f2238abb9d
Adam Buechler <butchler@gmail.com>
2017-09-17 00:01:40 +0900

    Use mocha reporter with diffs

--- packages/s-is-for-style/karma.conf.js
+++ packages/s-is-for-style/karma.conf.js
@@ -35,8 +35,11 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
+    mochaReporter: {
+      showDiff: true,
+    },
 
     // web server port
     port: 9876,
