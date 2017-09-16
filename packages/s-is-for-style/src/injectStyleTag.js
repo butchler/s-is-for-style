2b8f327c2f5e1f25452aacf1b7e3aa774a084a0e
Adam Buechler <butchler@gmail.com>
2017-09-16 22:14:31 +0900

    Remove old style tags before injecting new ones

--- packages/s-is-for-style/src/injectStyleTag.js
+++ packages/s-is-for-style/src/injectStyleTag.js
@@ -1,8 +1,15 @@
 const injectStyleTag = () => {
-  // TODO: Add data attribute.
-  // TODO: Remove any existing server-rendered sheets.
+  // Remove any existing sheets.
+  // TODO: Make selector configurable, and add configurable classname prefix.
+  const oldTags = [...document.querySelectorAll('[data-s-sheet=true]')];
+  while (oldTags.length > 0) {
+    const element = oldTags.pop();
+    element.parentNode.removeChild(element);
+  }
+
   const styleTag = document.createElement('style');
   styleTag.setAttribute('type', 'text/css');
+  styleTag.setAttribute('data-s-sheet', true);
   document.head.appendChild(styleTag);
   return styleTag;
 };
